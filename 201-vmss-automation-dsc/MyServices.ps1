2cf11adbed81af0993a83b0b5c346de9bac4ba9f
shilparamky5 <34554999+shilparamky5@users.noreply.github.com>
2017-12-28 11:58:35 +1300

    Update MyServices.ps1

--- 201-vmss-automation-dsc/MyServices.ps1
+++ 201-vmss-automation-dsc/MyServices.ps1
@@ -1,53 +1,104 @@
 Configuration MyServices
+
 {
+
  Node "localhost"
+
   {
 
 
+
+
+
    	 WindowsFeature MessageQueueFeature
+
     	{
+
       	Ensure = "Present"
+
       	Name = "MSMQ"
+
     	}
+
      
-    
+
+    New-PSDrive -Name W -PSProvider FileSystem -root \\hedgebook.file.core.windows.net\configuration
+
+
 
         File DirectoryCopy
+
         {
+
             Ensure = "Present"  
+
             Type = "Directory" 
+
             Recurse = $true
-            New-PSDrive -Name W -PSProvider FileSystem -root \\hedgebook.file.core.windows.net\configuration
+
+
             SourcePath = "file:\\\W:\configuration\"
+
             DestinationPath = "C:\Users"
+
         }
+
  
+
         Log AfterDirectoryCopy
+
         {
+
             # The message below gets written to the Microsoft-Windows-Desired State Configuration/Analytic log
+
             Message = "Finished copying the PAS files with ID DirectoryCopy"
+
             DependsOn = "[File]DirectoryCopy" # This means run "DirectoryCopy" first.
+
         }
+
         
+
         archive ZipFile {
+
         Path = "C:\Users\isa_services.zip"
+
         Destination = "C:\Users"
+
         Ensure = 'Present'
+
  
+
         }
+
+
 
         Service PasService {
 
+
+
             Name = "PAS Service"
+
             BuiltInAccount = "LocalSystem"
+
             Path = "C:\Users\isa_services\Pas.exe"
+
             StartupType = "Manual"
+
             State       = "Running"
+
+
 
         }
 
 
 
+
+
+
+
 }
+
 }
+
 MyServices

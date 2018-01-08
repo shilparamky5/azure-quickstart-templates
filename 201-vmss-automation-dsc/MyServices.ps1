032acd40973810da03b975f0533f4ff55f7bc836
shilparamky5 <34554999+shilparamky5@users.noreply.github.com>
2018-01-08 16:31:55 +1300

    Update MyServices.ps1

--- 201-vmss-automation-dsc/MyServices.ps1
+++ 201-vmss-automation-dsc/MyServices.ps1
@@ -1,61 +1,28 @@
 Configuration MyServices
 
-
-
 {
-
-
 
 Import-DscResource –ModuleName 'PSDesiredStateConfiguration' 
 
-
-
- Node "localhost"
-
-
+Node "localhost"
 
   {
 
-
-
-
-
    	 WindowsFeature MessageQueueFeature
-
-
-
     	{
 
-
-
-      	Ensure = "Present"
-
-
-
+     	Ensure = "Present"
       	Name = "MSMQ"
-
-
-
-    	}
+   	}
  
  
- Credential AZURE\hedgebook{
-    $password = "Ra9T3Gmz0wE4OHIVHTJJvi8X6C1nuC1oJIZrGWPMI+edW6EbLp3Hw6qmWQsodu00s0nv7SiB/wa1DvkLzS7xQA==" | ConvertTo-SecureString -asPlainText -Force
- }
 
-    [PSCredential] $credential = Get-AzureRmAutomationCredential -ResourceGroupName "MarkToMarket" -AutomationAccountName "myAutomationAccount" -Name "AZURE\hedgebook" -Value $password
-
-
-   
- 
- 
+$acctKey = ConvertTo-SecureString -String "Ra9T3Gmz0wE4OHIVHTJJvi8X6C1nuC1oJIZrGWPMI+edW6EbLp3Hw6qmWQsodu00s0nv7SiB/wa1DvkLzS7xQA==" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\hedgebook", $acctKey
+New-PSDrive -Name Z -PSProvider FileSystem -Root "\\hedgebook.file.core.windows.net\configuration" -Credential $credential -Persist
    
 
-        
-
-  File DirectoryCopy
-
-         
+   File DirectoryCopy
 
     {
 
@@ -68,124 +35,39 @@ Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
          Type = "Directory"
 
          Recurse = $true
-
-         Credential = $credential
    
    }
  
 
-
-
         Log AfterDirectoryCopy
-
-
 
         {
 
-
-
             # The message below gets written to the Microsoft-Windows-Desired State Configuration/Analytic log
 
-
-
             Message = "Finished copying the PAS files with ID DirectoryCopy"
-
-
-
             DependsOn = "[File]DirectoryCopy" # This means run "DirectoryCopy" first.
-
-
-
         }
-
-
-
         
-
-
-
         archive ZipFile {
 
-
-
         Path = "C:\Users\isa_services.zip"
-
-
-
         Destination = "C:\Users"
-
-
-
         Ensure = 'Present'
-
-
-
- 
-
-
-
         }
-
-
-
-
-
 
 
         Service PasService {
 
-
-
-
-
-
-
             Name = "PAS Service"
-
-
-
             BuiltInAccount = "LocalSystem"
-
-
-
             Path = "C:\Users\isa_services\Pas.exe"
-
-
-
             StartupType = "Manual"
-
-
-
             State       = "Running"
-
-
-
-
-
-
-
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
-
-
 }
-
-
 
 MyServices
